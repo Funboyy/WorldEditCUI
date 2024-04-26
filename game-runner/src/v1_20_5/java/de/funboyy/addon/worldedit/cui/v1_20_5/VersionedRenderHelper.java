44fc747830638fa35193dada3c89c158be0c2d01
Funboyy <72627658+Funboyy@users.noreply.github.com>
2024-04-26 10:29:53 +0200

    added 1.20.5 support

---
+++ game-runner/src/v1_20_5/java/de/funboyy/addon/worldedit/cui/v1_20_5/VersionedRenderHelper.java
@@ -0,0 +1,46 @@
+package de.funboyy.addon.worldedit.cui.v1_20_5;
+
+import com.mojang.blaze3d.vertex.Tesselator;
+import de.funboyy.addon.worldedit.cui.api.render.RenderHelper;
+import net.labymod.api.models.Implements;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.ShaderInstance;
+
+@Implements(RenderHelper.class)
+public class VersionedRenderHelper implements RenderHelper {
+
+  @Override
+  public Object getShader() {
+    return com.mojang.blaze3d.systems.RenderSystem.getShader();
+  }
+
+  @Override
+  public void setShader(final Object object) {
+    if (!(object instanceof ShaderInstance shader)) {
+      throw new IllegalArgumentException("You can only set a ShaderInstance as shader");
+    }
+
+    com.mojang.blaze3d.systems.RenderSystem.setShader(() -> shader);
+  }
+
+  @Override
+  public Object getPositionColorShader() {
+    return GameRenderer.getPositionColorShader();
+  }
+
+  @Override
+  public Object getRenderTypeLinesShader() {
+    return GameRenderer.getRendertypeLinesShader();
+  }
+
+  @Override
+  public void endTesselator() {
+    Tesselator.getInstance().end();
+  }
+
+  @Override
+  public void applyModelViewMatrix() {
+    com.mojang.blaze3d.systems.RenderSystem.applyModelViewMatrix();
+  }
+
+}
