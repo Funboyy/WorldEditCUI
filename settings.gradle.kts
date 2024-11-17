ba17cfc94644f19d1373ec62715ed359a51b25a4
Funboyy <72627658+Funboyy@users.noreply.github.com>
2024-11-17 11:14:05 +0100

    update LabyGradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "worldeditcui"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.5"
+    val labyGradlePluginVersion = "0.5.6"
 
     buildscript {
         repositories {
