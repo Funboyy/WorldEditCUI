a161fd0cce5f15785cb6e19d893f6a63536ba4d8
Funboyy <72627658+Funboyy@users.noreply.github.com>
2026-06-04 14:14:50 +0200

    updated LabyGradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "worldeditcui"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.8.0"
+    val labyGradlePluginVersion = "0.8.1"
 
     buildscript {
         repositories {
@@ -13,7 +13,7 @@ pluginManagement {
         }
 
         dependencies {
-            classpath("net.labymod.gradle", "common", labyGradlePluginVersion)
+            classpath("net.labymod.gradle:common:$labyGradlePluginVersion")
         }
     }
 }
